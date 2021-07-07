ac4ae83ba46378986436967c0b3134637ae4e9ee
Charles <charles.donven@tum.de>
2021-07-07 17:29:18 +0200

    Clean Repository

--- MQTT/Publisher/sketch_may30a/WifiAccess.h
+++
@@ -1,6 +0,0 @@
-#ifndef _MYEXT_H
-#define _MYEXT_H
-const char* ssid_name = "WG01";
-const char* password_name = "321losgehts";
-const char* server_ip = "192.168.178.100";
-#endif
---
+++ Project Code/Publisher/gloveESP32/WifiAccess.h
@@ -0,0 +1,6 @@
+#ifndef _MYEXT_H
+#define _MYEXT_H
+const char* ssid_name = "FRITZ!Box Donven";
+const char* password_name = "MerciCharel:)";
+const char* server_ip = "192.168.178.27";
+#endif
