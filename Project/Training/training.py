d2210d87c2f01d09e6994f331bb68188fc1fba5d
yucong.ma <yucong.ma@tum.de>
2021-07-11 13:14:39 +0200

    Compared different svm kernels and random forest

--- Project/Training/training.py
+++ Project/Training/training.py
@@ -14,8 +14,11 @@ import pydot
 import os
 
 class SVMModel:
-    sensor_data_matrix = np.zeros([100, 90])
-    classifier = svm.SVC(kernel = 'poly')
+    sensor_data_matrix = np.zeros([400, 90])
+    classifier_linear = svm.SVC(kernel = 'linear')
+    classifier_poly = svm.SVC(kernel = 'poly')
+    classifier_rbf = svm.SVC(kernel = 'rbf')
+    classifier_sigmoid = svm.SVC(kernel = 'sigmoid')
 
     def get_gesture_prediction(self, filename):
         sensor_data = pandas.read_csv(filename, header = 0)
@@ -31,45 +34,78 @@ class SVMModel:
     def training(self):
         self.read_data_from_csv("left", 0)
         self.read_data_from_csv("right", 1)
+        self.read_data_from_csv("clock", 1)
+        self.read_data_from_csv("counter", 1)
 
         # hardcoded target for gesture labeling. left swiping is 1, right swiping is 2
-        target = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                           1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                           1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                           1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                           1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                           2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-                           2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-                           2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-                           2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-                           2, 2, 2, 2, 2, 2, 2, 2, 2, 2])
+        target = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                           1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                           1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                           1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                           1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                           2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+                           2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+                           2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+                           2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+                           2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+                           3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+                           3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+                           3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+                           3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+                           3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+                           4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+                           4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+                           4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+                           4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+                           4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4])
 
         X_train, X_test, y_train, y_test = train_test_split(SVMModel.sensor_data_matrix, target, test_size=0.3, random_state=42)
-        SVMModel.classifier.fit(X_train, y_train)
-        y_prediction = SVMModel.classifier.predict(X_test)
-        print("SVM accuracy is ", metrics.accuracy_score(y_test, y_prediction))
-
+        # SVM linear
+        SVMModel.classifier_linear.fit(X_train, y_train)
+        y_prediction = SVMModel.classifier_linear.predict(X_test)
+        print("SVM linear kernel accuracy is ", metrics.accuracy_score(y_test, y_prediction))
+        
+        # SVM poly
+        SVMModel.classifier_poly.fit(X_train, y_train)
+        y_prediction = SVMModel.classifier_poly.predict(X_test)
+        print("SVM poly kernel accuracy is ", metrics.accuracy_score(y_test, y_prediction))
+        
+        # SVM rbf
+        SVMModel.classifier_rbf.fit(X_train, y_train)
+        y_prediction = SVMModel.classifier_rbf.predict(X_test)
+        print("SVM rbf kernel accuracy is ", metrics.accuracy_score(y_test, y_prediction))
+        
+        # SVM sigmoid
+        SVMModel.classifier_sigmoid.fit(X_train, y_train)
+        y_prediction = SVMModel.classifier_sigmoid.predict(X_test)
+        print("SVM sigmoid kernel accuracy is ", metrics.accuracy_score(y_test, y_prediction))
+        
         # Random Forest
         train_features, test_features, train_labels, test_labels = train_test_split(SVMModel.sensor_data_matrix, target, 
-                                                                                    test_size = 0.25, random_state = 42)
-        rf = RandomForestRegressor(n_estimators=10)
+                                                                                    test_size = 0.3, random_state = 42)
+        
+        # Normal tree
+        rf = RandomForestRegressor(n_estimators = 1000, random_state = 42)
         rf.fit(train_features, train_labels)
         predictions = rf.predict(test_features)
-        print(test_labels)
-        print(predictions)
         errors = abs(predictions - test_labels)
-        print('Mean Absolute Error is: ', round(np.mean(errors), 2), '.')
+        print('Random forest mean absolute error is: ', round(np.mean(errors), 2))
         mape = 100 * (errors / test_labels)
         accuracy = 100 - np.mean(mape)
-        print('Random forest accuracy is :', round(accuracy, 2), '%.')
+        print('Random forest accuracy is :', round(accuracy, 2), '%')
 
-        #tree = rf.estimators_[5]
-        #export_graphviz(tree, out_file = 'tree.dot', rounded = True, precision = 1)
-        #(graph, ) = pydot.graph_from_dot_file('tree.dot')
-        #graph.write_png('tree.png')
+        # Small tree
+        rf_small = RandomForestRegressor(n_estimators = 10, max_depth = 3, random_state = 42)
+        rf_small.fit(train_features, train_labels)
+        predictions = rf_small.predict(test_features)
+        errors = abs(predictions - test_labels)
+        print('Random forest small tree mean absolute error is: ', round(np.mean(errors), 2))
+        mape = 100 * (errors / test_labels)
+        accuracy = 100 - np.mean(mape)
+        print('Random forest small tree accuracy is :', round(accuracy, 2), '%')
 
-        filename = 'svm_model.sav'
-        pickle.dump(SVMModel.classifier, open(filename, 'wb'))
+        # filename = 'svm_model.sav'
+        # pickle.dump(SVMModel.classifier, open(filename, 'wb'))
         
         #loaded_model = pickle.load(open(filename, 'rb'))
         #y_prediction2 = loaded_model.predict(X_test)
@@ -77,7 +113,7 @@ class SVMModel:
         return metrics.accuracy_score(y_test, y_prediction)
 
     def read_data_from_csv(self, gesture, index_offset):
-        index = 0 + 50 * index_offset
+        index = 0 + 100 * index_offset
         for file in glob.glob("/home/pi/smart-glove-control-system/Project/Training/" + gesture + "/*.csv"):
             sensor_data = pandas.read_csv(file, header = 0)
             row = np.concatenate((np.array(sensor_data['yaw']).T,
