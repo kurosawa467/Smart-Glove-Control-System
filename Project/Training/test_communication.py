6ed8ec9bff021ce2477faf03fd6d7b228053c21e
yucong.ma <yucong.ma@tum.de>
2021-07-11 16:30:21 +0200

    Updated test_communication.py

--- Project/Training/test_communication.py
+++ Project/Training/test_communication.py
@@ -6,114 +6,160 @@ import errno
 import datetime
 import pandas
 from training import SVMModel
+import numpy as np
+import pickle
 
 MQTT_ADDRESS = '192.168.178.100'
 MQTT_USER = 'mosquitto'
 MQTT_PASSWORD = 'mosquitto'
 GLOVE_TOPIC = '/esp32/glove'
+IoT_TOPIC = ['/device1', '/device2', '/device3']
 dim = 50
 direction = 1
 encoding = 'utf-8'
 gesture = 'gesture'
 header = ["timestamp", "yaw", "pitch", "row"]
-start_time = 0
 sensor_data = []
+sensor_data_matrix = []
 filename_prefix = 'gesture_'
 filename_index = 0
 message_index = 0
-svmModel = None
-gesture_mode = False
+start_time = 0
+rf = None
+rf_small = None
+gesture_mode = 1
 
 def on_connect(client, userdata, flags, rc):
-  global svmModel
-  global start_time
-  print('Connected with ESP32, result: ' + str(rc))
-  client.subscribe(GLOVE_TOPIC)
-  if start_time == 0:
+    global rf
+    global rf_small
+    global start_time
+    print('Connected with ESP32, result: ' + str(rc))
+    client.subscribe(GLOVE_TOPIC)
+    rf = pickle.load(open('random_forest.sav', 'rb'))
+    rf_small = pickle.load(open('random_forest_small.sav', 'rb'))
+    print('Random forest model ready')
     start_time = datetime.datetime.now()
-  print(start_time)
-  svmModel = SVMModel()
-  accuracy = svmModel.training()
-  print('SVM model is ready, initial accuracy is ' + str(accuracy))
+    print()
 
 
 def on_message(client, userdata, msg):
-  global start_time
-  global message_index
-  global filename_index
-  global gesture_mode
-  global sensor_data
-  # print('Message topic: ' + msg.topic + ', message payload: ' + str(msg.payload))
-  raw_message = str(msg.payload)
-  message = raw_message[raw_message.index('=>') + 2:].rstrip("'")
-  tokens = message.split(',')
+    global start_time
+    global message_index
+    global filename_index
+    global gesture_mode
+    global sensor_data
+    # print('Message topic: ' + msg.topic + ', message payload: ' + str(msg.payload))
+    raw_message = str(msg.payload)
+    message = raw_message[raw_message.index('=>') + 2:].rstrip("'")
+    tokens = message.split(',')
 
-  hand = get_finger_positions(tokens[:4])
+    hand = get_finger_positions(tokens[:4])
 
-  # Here decides when to start recording gesture
-  if not gesture_mode:
-      if hand==15:
-          gesture_mode = True
+    # Here decides when to start recording gesture
+    if gesture_mode == 1:
+        if hand == 1 or hand == 3:
+            gesture_mode = 2
+    if gesture_mode == 3:
+        if hand == 15:
+            gesture_mode = 1
 
-  if gesture_mode:
-    if message_index == 0:
-      print('Start to recognize gesture')
-      sensor_data = []
-    if message_index < 30:
-      write_to_matrix(tokens[5:8])
-      message_index += 1
-    if message_index == 30:
-      gesture = get_gesture_prediction()
-      print(gesture)
-      command = ''
-      if hand == 1:
-          if gesture == 1:
-              command = 'next device'
-          elif gesture == 2:
-              command = 'previous device'
-      elif hand == 3:
-          if gesture == 1:
-              command = 'next color'
-          elif gesture == 2:
-              command = 'previous color'
-      print('Recognized command is: ' + command)
-      message_index = 0
-      filename_index += 1
-      gesture_mode = False
+    if gesture_mode == 2:
+        if message_index == 0:
+            print('Start to recognize gesture')
+            sensor_data = []
+        if message_index < 30:
+            write_to_matrix(tokens[5:8])
+            message_index += 1
+        if message_index == 30:
+            start_time = datetime.datetime.now()
+            gesture = get_gesture_prediction()
+            end_time = datetime.datetime.now()
+            print(gesture)
+            rf_time = (end_time - start_time).total_seconds()
+            print(rf_time)
+            command = 'gesture unrecognized'
+            if hand == 1:
+                if gesture == 1:
+                    command = 'next device'
+                elif gesture == 2:
+                    command = 'previous device'
+                else:
+                    command = 'gesture unrecognized'
+            elif hand == 3:
+                if gesture == 1:
+                    command = 'next color'
+                elif gesture == 2:
+                    command = 'previous color'
+                elif gesture == 3:
+                    command = 'higher brightness'
+                elif gesture == 4:
+                    command = 'lower brightness'
+                else:
+                    command = 'gesture unrecognized'
+            print('Recognized command is: ' + command)
+            message_index = 0
+            filename_index += 1
+            gesture_mode = 3
 
 def write_to_matrix(tokens):
-  global sensor_data
-  current_time = (datetime.datetime.now() - start_time).total_seconds() * 1000
-  row = [current_time]
-  row += tokens
-  sensor_data.append(row)
+    global sensor_data
+    current_time = (datetime.datetime.now() - start_time).total_seconds() * 1000
+    row = [current_time]
+    row += tokens
+    sensor_data.append(row)
+  
+def matrix_transpose_and_flatten():
+    global sensor_data
+    global sensor_data_matrix
+    sensor_data_matrix = np.concatenate(np.array(sensor_data).transpose())[30:]
+  
 
 def write_to_csv():
-  dataframe = pandas.DataFrame(sensor_data, columns = header)
-  dataframe.to_csv('user/gesture_' + str(filename_index) + '.csv', header = True)
+    dataframe = pandas.DataFrame(sensor_data, columns = header)
+    dataframe.to_csv('user/gesture_' + str(filename_index) + '.csv', header = True)
 
 def get_machine_learning_prediction():
-  return svmModel.get_gesture_prediction('user/gesture_' + str(filename_index) + '.csv')
+    global sensor_data_matrix
+    global rf
+    user_sensor_data_matrix[0, :] = sensor_data_matrix
+    sensor_data_matrix = []
+    prediction = rf.predict(user_sensor_data_matrix)
+    return prediction[0]
 
 def get_gesture_prediction():
-  write_to_csv()
-  return get_machine_learning_prediction()
+    matrix_transpose_and_flatten()
+    prediction = get_machine_learning_prediction()
+    return evaluated_prediction(prediction)
 
 #hand position is decoded as 4 bit using the pinkie finger as the most significant bit
 def get_finger_positions(fingers):
-  hand = (1 if int(float(fingers[0])) <= 40 else 0) + (2 if int(float(fingers[1])) <= 40 else 0) + (4 if int(float(fingers[2])) <= 40 else 0) + (8 if int(float(fingers[3])) <= 40 else 0)
-  return hand
+    hand = (1 if int(float(fingers[0])) <= 40 else 0) + (2 if int(float(fingers[1])) <= 40 else 0) + (4 if int(float(fingers[2])) <= 40 else 0) + (8 if int(float(fingers[3])) <= 40 else 0)
+    return hand
+
+def evaluated_prediction(prediction):
+    gesture = 0
+    if prediction >= 0.9 and prediction <= 1.1:
+        gesture = 1
+    elif prediction >= 1.9 and prediction <= 2.1:
+        gesture = 2
+    elif prediction >= 2.9 and prediction <= 3.1:
+        gesture = 3
+    elif prediction >= 3.9 and prediction <= 4.1:
+        gesture = 4
+    else:
+        gesture = 0
+    return gesture
 
 
 def main(): 
-  mqtt_client = mqtt.Client()
-  mqtt_client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
-  mqtt_client.on_connect = on_connect
-  mqtt_client.on_message = on_message
+    mqtt_client = mqtt.Client()
+    mqtt_client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
 
-  mqtt_client.connect(MQTT_ADDRESS, 1883)
-  mqtt_client.loop_forever()
+    mqtt_client.connect(MQTT_ADDRESS, 1883)
+    mqtt_client.loop_forever()
 
 if __name__ == '__main__':
-  print('main function')
-  main()
+    print('main function')
+    main()
--- Project/Training/training.py
+++ Project/Training/training.py
@@ -9,9 +9,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 import pickle
-from sklearn.tree import export_graphviz
-import pydot
-import os
+import datetime
 
 class SVMModel:
     sensor_data_matrix = np.zeros([400, 90])
@@ -37,7 +35,7 @@ class SVMModel:
         self.read_data_from_csv("clock", 1)
         self.read_data_from_csv("counter", 1)
 
-        # hardcoded target for gesture labeling. left swiping is 1, right swiping is 2
+        # hardcoded target for gesture labeling. left swiping is 1, right swiping is 2, clockwise circle is 3, counter-clockwise circle is 4
         target = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
                            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
                            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -61,51 +59,66 @@ class SVMModel:
 
         X_train, X_test, y_train, y_test = train_test_split(SVMModel.sensor_data_matrix, target, test_size=0.3, random_state=42)
         # SVM linear
+        SVM_linear_start_time = datetime.datetime.now()
         SVMModel.classifier_linear.fit(X_train, y_train)
         y_prediction = SVMModel.classifier_linear.predict(X_test)
-        print("SVM linear kernel accuracy is ", metrics.accuracy_score(y_test, y_prediction))
-        
-        # SVM poly
-        SVMModel.classifier_poly.fit(X_train, y_train)
-        y_prediction = SVMModel.classifier_poly.predict(X_test)
-        print("SVM poly kernel accuracy is ", metrics.accuracy_score(y_test, y_prediction))
+        accuracy = metrics.accuracy_score(y_test, y_prediction)
+        SVM_linear_end_time = datetime.datetime.now()
+        SVM_linear_time = (SVM_linear_end_time - SVM_linear_start_time).total_seconds()
+        print("SVM linear kernel accuracy is:", round(accuracy * 100, 4), '%')
+        print("SVM linear kernel takes ", round(SVM_linear_time, 4), "seconds")
+        print()
         
         # SVM rbf
+        SVM_non_linear_start_time = datetime.datetime.now()
         SVMModel.classifier_rbf.fit(X_train, y_train)
         y_prediction = SVMModel.classifier_rbf.predict(X_test)
-        print("SVM rbf kernel accuracy is ", metrics.accuracy_score(y_test, y_prediction))
-        
-        # SVM sigmoid
-        SVMModel.classifier_sigmoid.fit(X_train, y_train)
-        y_prediction = SVMModel.classifier_sigmoid.predict(X_test)
-        print("SVM sigmoid kernel accuracy is ", metrics.accuracy_score(y_test, y_prediction))
+        accuracy = metrics.accuracy_score(y_test, y_prediction)
+        SVM_non_linear_end_time = datetime.datetime.now()
+        SVM_non_linear_time = (SVM_non_linear_end_time - SVM_non_linear_start_time).total_seconds()
+        print("SVM non-linear (rbf) kernel accuracy is:", round(accuracy * 100, 4), '%')
+        print("SVM non-linear kernel takes ", round(SVM_non_linear_time, 4), "seconds")
+        print()
         
         # Random Forest
         train_features, test_features, train_labels, test_labels = train_test_split(SVMModel.sensor_data_matrix, target, 
                                                                                     test_size = 0.3, random_state = 42)
         
         # Normal tree
+        random_forest_start_time = datetime.datetime.now()
         rf = RandomForestRegressor(n_estimators = 1000, random_state = 42)
         rf.fit(train_features, train_labels)
         predictions = rf.predict(test_features)
         errors = abs(predictions - test_labels)
-        print('Random forest mean absolute error is: ', round(np.mean(errors), 2))
         mape = 100 * (errors / test_labels)
         accuracy = 100 - np.mean(mape)
-        print('Random forest accuracy is :', round(accuracy, 2), '%')
+        random_forest_end_time = datetime.datetime.now()
+        random_forest_time = (random_forest_end_time - random_forest_start_time).total_seconds()
+        print('Random forest mean absolute error is:', round(np.mean(errors), 2))
+        print('Random forest accuracy is:', round(accuracy, 4), '%')
+        print("Random forest takes ", round(random_forest_time, 4), "seconds")
+        print()
 
         # Small tree
-        rf_small = RandomForestRegressor(n_estimators = 10, max_depth = 3, random_state = 42)
+        random_forest_small_start_time = datetime.datetime.now()
+        rf_small = RandomForestRegressor(n_estimators = 50, max_depth = 3, random_state = 42)
         rf_small.fit(train_features, train_labels)
         predictions = rf_small.predict(test_features)
         errors = abs(predictions - test_labels)
-        print('Random forest small tree mean absolute error is: ', round(np.mean(errors), 2))
         mape = 100 * (errors / test_labels)
         accuracy = 100 - np.mean(mape)
-        print('Random forest small tree accuracy is :', round(accuracy, 2), '%')
+        random_forest_small_end_time = datetime.datetime.now()
+        random_forest_small_time = (random_forest_small_end_time - random_forest_small_start_time).total_seconds()
+        print('Random forest small tree mean absolute error is:', round(np.mean(errors), 2))
+        print('Random forest small tree accuracy is:', round(accuracy, 4), '%')
+        print("Random forest small tree takes ", round(random_forest_small_time, 4), "seconds")
+        print()
 
-        # filename = 'svm_model.sav'
-        # pickle.dump(SVMModel.classifier, open(filename, 'wb'))
+        rf_filename = 'random_forest.sav'
+        pickle.dump(rf, open(rf_filename, 'wb'))
+        
+        rf_small_filename = 'random_forest_small.sav'
+        pickle.dump(rf_small, open(rf_small_filename, 'wb'))
         
         #loaded_model = pickle.load(open(filename, 'rb'))
         #y_prediction2 = loaded_model.predict(X_test)
