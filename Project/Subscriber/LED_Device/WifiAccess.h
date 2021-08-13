3e3f0d01d5e2f3f23a2ce2967f23666d31ca4699
Charles Donven <charles.donven@tum.de>
2021-08-13 09:59:12 +0000

    remove personel settings

--- Project/Subscriber/LED_Device/WifiAccess.h
+++ Project/Subscriber/LED_Device/WifiAccess.h
@@ -1,6 +1,6 @@
 #ifndef _MYEXT_H
 #define _MYEXT_H
-const char* ssid_name = "FRITZ!Box Donven";
-const char* password_name = "MerciCharel:)";
-const char* server_ip = "192.168.178.27";
+const char* ssid_name = "";
+const char* password_name = "";
+const char* server_ip = "";
 #endif
