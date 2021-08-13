651b29b9b6f07d5f743643b733e5c7e435fda502
Charles Donven <charles.donven@tum.de>
2021-08-13 09:59:56 +0000

    Update WifiAccess.h

--- Project/Publisher/gloveESP32/WifiAccess.h
+++ Project/Publisher/gloveESP32/WifiAccess.h
@@ -1,6 +1,6 @@
 #ifndef _MYEXT_H
 #define _MYEXT_H
-const char* ssid_name = ***;
-const char* password_name = ***;
-const char* server_ip = "192.168.178.100";
+const char* ssid_name = "";
+const char* password_name = "";
+const char* server_ip = "";
 #endif
